d8b69b9e0ba48f6fc6d5fc383dde945d3b5ecfa8
Arnav Tyagi <tarnav18@gmail.com>
2026-09-03 12:14:22 +0530

    Rest operator + Arrow functions + String template

--- A.js
+++ A.js
@@ -48,32 +48,138 @@
 // s1.display();
 // s2.display();
 
-let product = {
-  productId: 101,
-  productName: "Laptop",
-  price: 50000,
-  quantity: 2,
+// let product = {
+//   productId: 101,
+//   productName: "Laptop",
+//   price: 50000,
+//   quantity: 2,
 
-  calculateTotalPrice: function () {
-    return this.price * this.quantity;
-  },
+//   calculateTotalPrice: function () {
+//     return this.price * this.quantity;
+//   },
 
-  updateQuantity: function (newQuantity) {
-    this.quantity = newQuantity;
-  },
+//   updateQuantity: function (newQuantity) {
+//     this.quantity = newQuantity;
+//   },
 
-  displayInfo: function () {
-    console.log("Product ID:", this.productId);
-    console.log("Product Name:", this.productName);
-    console.log("Price:", this.price);
-    console.log("Quantity:", this.quantity);
-    console.log("Total Price:", this.calculateTotalPrice());
-  },
+//   displayInfo: function () {
+//     console.log("Product ID:", this.productId);
+//     console.log("Product Name:", this.productName);
+//     console.log("Price:", this.price);
+//     console.log("Quantity:", this.quantity);
+//     console.log("Total Price:", this.calculateTotalPrice());
+//   },
+// };
+
+// product.displayInfo();
+
+// product.updateQuantity(5);
+
+// console.log("\nAfter updating quantity:");
+// product.displayInfo();
+
+let firstName = "Vikas";
+let lastName = "Choudhary";
+console.log("My first name is " + firstName);
+console.log("My last name is " + lastName);
+
+console.log(`My first name is ${firstName}. My last name is ${lastName}`);
+console.log(`My name is : ${firstName} ${lastName}`);
+console.log(`"My name is " ${firstName} ${lastName}`);
+// function fullName(first, last) {
+//   console.log(first);
+//   console.log(last);
+// }
+function fullName(first, last) {
+  return `${first} ${last}`;
+}
+// let name = new fullName(firstName, lastName);
+let name = `Hello ${fullName(firstName, lastName)}`;
+console.log(name);
+
+// function Hello() {
+//   console.log("Hello Vikas");
+// }
+
+let Hello = function () {
+  console.log("Hello Vikas");
 };
 
-product.displayInfo();
+Hello();
 
-product.updateQuantity(5);
+let Hello2 = () => console.log("Hello Vikas");
 
-console.log("\nAfter updating quantity:");
-product.displayInfo();
+Hello2();
+
+function Physics(marks) {
+  return `${marks}`;
+}
+
+console.log(Physics(98));
+
+let Physics2 = (marks) => {
+  return `${marks}`;
+};
+
+console.log(Physics2(98));
+
+let fullName2 = (first, last) => {
+  return `${first} ${last}`;
+};
+
+console.log(fullName2(firstName, lastName));
+
+Star = (n) => {
+  let star = "\n";
+  for (let i = 0; i < n; i++) {
+    for (let j = 0; j <= i; j++) {
+      star += "* ";
+    }
+    star += "\n";
+  }
+  console.log(star);
+};
+
+Pattern = (n) => {
+  pattern = "\n";
+  for (let i = 1; i <= n; i++) {
+    for (let j = 1; j <= i; j++) {
+      pattern += j;
+    }
+    pattern += "\n";
+  }
+  console.log(pattern);
+};
+
+Star(3);
+Pattern(3);
+
+function sum(...values) {
+  console.log(values);
+}
+
+sum(10, 20);
+sum(10, 100, 1000);
+
+function sum2(...values) {
+  let total = 0;
+  for (let n of values) {
+    total += n;
+  }
+  return total;
+}
+
+console.log(sum2(10, 20, 30));
+console.log(sum2(10, 20, 30, 40));
+
+function sum3(first, last, ...values) {
+  let total = 0;
+  for (let n of values) {
+    total += n;
+  }
+  let name = first + " " + last;
+  console.log("name : ", name);
+  console.log("total : ", total);
+}
+
+sum3("Arnav", "Tyagi", (10, 20, 30, 40));
